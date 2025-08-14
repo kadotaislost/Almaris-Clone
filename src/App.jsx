bb797024d3281df1f67660c5c4de63d0b731a997
Sachin Lama <sachin2211068@iimscollege.edu.np>
2025-08-14 22:15:23 +0545

    initial commit. start project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-4xl font text-blue-300'>Welcome to Almaris</h1>
+    </>
+  )
+}
+
+export default App
